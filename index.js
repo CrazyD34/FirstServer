3291b36172e891e21b93baeb2d6f2ebee67fa64e
David G <pericopter67@gmail.com>
2023-04-29 18:46:53 -0600

    📢Formulario + Envio de mensaje

--- index.js
+++ index.js
@@ -16,17 +16,61 @@ const server = http.createServer(async (req, res) => {
     case '/':
       // Peticion raiz
       // Estableciendo cabeceras
-      res.setHeader('Content-Type', 'text/html');
+      res.setHeader('Content-Type','text/html');
       // Escribiendo la respuesta
       res.write(`
       <html>
         <head>
         <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
           <title>My App</title>
+        <style>
+          body{
+            background-color: #ECF0F1;
+            font-family: Arial,sans-serif;
+          }
+          h1,h2{
+            color: #3498DB;
+            text-align: center;
+            margin-top: 50px;
+          }
+          form{
+            margin-top: 30px;
+            text-align:center;
+          }
+          input[type="text"]
+          {
+            width: 300px;
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+            box-shadow: 0px 0px 5px #3498DB;
+            outline: none;
+          }
+          button[type="submit"]
+          {
+            background-color: #3498DB;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            padding 10px 20px;
+            cursor: pointer;
+            box-shadow: 0px 0px 5px #3498DB;
+            outline: none;
+          }
+          button[type="submit"]:hover{
+            background-color: #2980B9;
+          }
+        </style>
         </head>
-        <body> 
-          <h1 style="color: #333">Hello from my server</h1>
-          <p style="color: #34495E">Estas en el recurso raiz.</p>
+        <body>
+        <h1> Hola desde mi servidor</h1>
+        <h2> Ingresa un mensaje </h2>
+        <div>
+          <form action="/message" method="POST">
+          <input type="text" name="message">
+          <button type="submit"> Enviar </button>
+          </form>
+        </div>
         </body>
       </html>
       `);
@@ -89,8 +133,78 @@ const server = http.createServer(async (req, res) => {
         // Cerrando la comunicacion
         res.end();
       }
-      break
-    default:
+    break;
+      //Agregando un BackEnd y un Endpoint para enviar informacion
+    case "/message":
+      // Verificando si es post
+      if (method === "POST") {
+		  // Se crea una variable para almacenar los datos del cliente
+      let body = "";
+      //--------------
+		  // Se registra un manejador de evento para la recepción de datos
+      req.on("data", (data => {
+      body += data;
+      if (body.length > 1e6) return req.socket.destroy();
+      }));
+      ///------------
+		  // Se registra una manejador de eventos para el termino de recepción de datos
+      req.on("end", () => {
+      // Procesa el formulario
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+			// Mediante URLSearchParams se extraen los campos del formulario
+      const params = new URLSearchParams(body);
+			// Se construye un objeto a partir de los datos en la variable params
+      const parsedParams = Object.fromEntries(params);
+      res.write(`
+      <html>
+        <head>
+          <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
+          <title>My App</title>
+          <style>
+            body{
+              background-color: #f9f9f9;
+              font-family: Arial, sans-serif;
+            }
+            h1{
+              color: #e74c3c;
+              font-size: 48px;
+              margin-top: 50px;
+              text-align: center;
+            }
+            p{
+              font-size: 24px;
+              color: #7f8c8d;
+              text-align:center;
+              margin-top: 20px;
+            }
+            .error-message{
+              font-size: 18px;
+              color: #95a5a6;
+              text-align:center;
+              margin-top: 20px;
+            }
+          </style>
+        </head>
+        <body>
+          <h1 style="color: #333">SERVER MESSAGE RECIEVED &#128172</h1>
+          <p>${parsedParams.message}</p>
+        </body>
+      </html>
+      `);
+			// Se termina la conexion
+      return res.end();
+     });
+     }
+     //En caso que no exista en endpoint se muestra el error 404 
+     else 
+     {
+      res.statusCode = 404;
+      res.write("404: Endpoint no encontrado")
+      res.end();
+     }
+  break;
+  default:
       // Peticion raiz
       // Estableciendo cabeceras
       res.setHeader('Content-Type', 'text/html');
